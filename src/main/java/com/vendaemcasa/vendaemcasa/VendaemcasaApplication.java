8afb37d75af927f402e2ed25ea998533092dc0d1
flaviofss07 <flaviofss.07@gmail.com>
2020-03-23 17:50:27 -0300

    Projeto Criado

---
+++ src/main/java/com/vendaemcasa/vendaemcasa/VendaemcasaApplication.java
@@ -0,0 +1,13 @@
+package com.vendaemcasa.vendaemcasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaemcasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaemcasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vendaemcasa/vendaemcasa/VendaemcasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendaemcasa.vendaemcasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaemcasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
