12037bcd049cc3a262193be9130f49dd3b8f330b
flaviofss07 <flaviofss.07@gmail.com>
2020-03-24 01:21:19 -0300

    Criando operação de instanciação

--- src/main/java/com/vendaemcasa/vendaemcasa/VendaemcasaApplication.java
+++ src/main/java/com/vendaemcasa/vendaemcasa/VendaemcasaApplication.java
@@ -1,13 +1,29 @@
 package com.vendaemcasa.vendaemcasa;
 
+import com.vendaemcasa.vendaemcasa.domain.Categoria;
+import com.vendaemcasa.vendaemcasa.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class VendaemcasaApplication {
+public class VendaemcasaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendaemcasaApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception{
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
