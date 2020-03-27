c479a1b665084b4b3b3abf56251f9b933ea496ef
flaviofss07 <flaviofss.07@gmail.com>
2020-03-27 18:06:25 -0300

    Ajustes no endpoint {id}

--- src/main/java/com/vendaemcasa/vendaemcasa/services/CategoriaService.java
+++ src/main/java/com/vendaemcasa/vendaemcasa/services/CategoriaService.java
@@ -2,6 +2,8 @@ package com.vendaemcasa.vendaemcasa.services;
 
 import com.vendaemcasa.vendaemcasa.domain.Categoria;
 import com.vendaemcasa.vendaemcasa.repositories.CategoriaRepository;
+
+import com.vendaemcasa.vendaemcasa.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,10 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id){
+    public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
 
