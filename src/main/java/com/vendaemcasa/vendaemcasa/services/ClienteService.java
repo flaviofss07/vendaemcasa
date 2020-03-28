24a1af00c20dcd25b5824937fafd9aef2d9d5ddf
flaviofss07 <flaviofss.07@gmail.com>
2020-03-28 00:33:16 -0300

    Endpoint cliente - {id} disponível

---
+++ src/main/java/com/vendaemcasa/vendaemcasa/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.vendaemcasa.vendaemcasa.services;
+
+
+import com.vendaemcasa.vendaemcasa.domain.Cliente;
+import com.vendaemcasa.vendaemcasa.repositories.ClienteRepository;
+import com.vendaemcasa.vendaemcasa.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+
+}
