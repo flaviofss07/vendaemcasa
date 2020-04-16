b7aa676b17de22f841c845d6b9b09c55a14070ad
flaviofss07 <flaviofss.07@gmail.com>
2020-04-16 00:54:35 -0300

    Paginaçcao com parametros opcionais

--- src/main/java/com/vendaemcasa/vendaemcasa/services/CategoriaService.java
+++ src/main/java/com/vendaemcasa/vendaemcasa/services/CategoriaService.java
@@ -5,8 +5,12 @@ import com.vendaemcasa.vendaemcasa.repositories.CategoriaRepository;
 
 import com.vendaemcasa.vendaemcasa.services.exceptions.DataIntegrityException;
 import com.vendaemcasa.vendaemcasa.services.exceptions.ObjectNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -45,6 +49,12 @@ public class CategoriaService {
     }
 
     public List<Categoria> findAll(){
+
         return repo.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
     }
 }
